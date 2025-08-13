cff3035203146e9c815eec8b1ef25c0624d9b842
oculo0204 <oculo0204@gmail.com>
2025-08-14 02:12:50 +0900

    refactor: #29 userutils

--- src/main/java/com/umc/linkyou/web/controller/AlarmController.java
+++ src/main/java/com/umc/linkyou/web/controller/AlarmController.java
@@ -1,24 +1,14 @@
 package com.umc.linkyou.web.controller;
 
 import com.umc.linkyou.apiPayload.ApiResponse;
-import com.umc.linkyou.apiPayload.code.status.ErrorStatus;
 import com.umc.linkyou.config.security.jwt.CustomUserDetails;
-import com.umc.linkyou.converter.LinkuConverter;
 import com.umc.linkyou.service.alarm.AlarmService;
-import com.umc.linkyou.service.category.CategoryService;
 import com.umc.linkyou.utils.UsersUtils;
-import com.umc.linkyou.web.dto.UserRequestDTO;
 import com.umc.linkyou.web.dto.alarm.AlarmRequestDTO;
-import com.umc.linkyou.web.dto.alarm.AlarmResponseDTO;
-import com.umc.linkyou.web.dto.category.CategoryListResponseDTO;
-import com.umc.linkyou.web.dto.linku.LinkuRequestDTO;
-import com.umc.linkyou.web.dto.linku.LinkuResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
