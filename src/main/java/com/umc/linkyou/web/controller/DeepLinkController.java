a5172b08aef4d520ca41fa98361588d45351922e
oculo0204 <oculo0204@gmail.com>
2025-10-25 04:31:53 +0900

    feat: #191 deeplink open url

---
+++ src/main/java/com/umc/linkyou/web/controller/DeepLinkController.java
@@ -0,0 +1,25 @@
+package com.umc.linkyou.web.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "deeplink-controller", description = "딥링크 리디렉션 페이지")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
+public class DeepLinkController {
+    @GetMapping("/open")
+    public String openPage(@RequestParam(value = "action", required = false) String action,
+                           @RequestParam(value = "folderId", required = false) String folderId,
+                           Model model) {
+        model.addAttribute("action", action);
+        model.addAttribute("folderId", folderId);
+        return "open"; // Thymeleaf 등 템플릿 엔진으로 open.html 렌더링
+    }
+
+}
