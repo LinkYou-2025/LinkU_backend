23f3d5ecce4263e388963cc0a99fffa39452d257
oculo0204 <oculo0204@gmail.com>
2025-11-05 04:56:53 +0900

    fix: deeplink url

--- src/main/java/com/umc/linkyou/web/controller/DeepLinkController.java
+++ src/main/java/com/umc/linkyou/web/controller/DeepLinkController.java
@@ -2,6 +2,7 @@ package com.umc.linkyou.web.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/")
 public class DeepLinkController {
+    
+    @Value("${app.deeplink.base-url}")
+    private String serverBaseUrl;
+    
     @GetMapping("/open")
     public String openPage(@RequestParam(value = "action", required = false) String action,
                            @RequestParam(value = "folderId", required = false) String folderId,
                            Model model) {
         model.addAttribute("action", action);
         model.addAttribute("folderId", folderId);
+        model.addAttribute("serverBaseUrl", serverBaseUrl);
         return "open"; // Thymeleaf 등 템플릿 엔진으로 open.html 렌더링
     }
 
