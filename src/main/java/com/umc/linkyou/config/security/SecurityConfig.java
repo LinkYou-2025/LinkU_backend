20f2769db21bc77145d43a50613ea482756c84ad
oculo0204 <oculo0204@gmail.com>
2026-02-06 07:16:14 +0900

    fix: #208 successhandler lazy loading

--- src/main/java/com/umc/linkyou/config/security/SecurityConfig.java
+++ src/main/java/com/umc/linkyou/config/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import com.umc.linkyou.oauth.OAuth2UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -29,6 +30,7 @@ public class SecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
     private final OAuth2UserServiceImpl oAuth2UserService;
     private final OAuth2TokenClient oAuth2TokenClient;
+    @Lazy
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
 
     @Bean
