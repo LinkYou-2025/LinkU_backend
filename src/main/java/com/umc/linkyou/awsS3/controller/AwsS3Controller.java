2fb76fc11008059c892daffedfadc79f22ed9398
oculo0204 <oculo0204@gmail.com>
2026-02-25 12:22:00 +0900

    fix: #240 syntax err

--- src/main/java/com/umc/linkyou/awsS3/controller/AwsS3Controller.java
+++ src/main/java/com/umc/linkyou/awsS3/controller/AwsS3Controller.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@@ApiV1
+@ApiV1
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/s3")
