0b7fc1d0d4e04b3c5ac64b8283da6afcd9adb19c
oculo0204 <oculo0204@gmail.com>
2026-03-05 16:43:38 +0900

    chore #247 error status

--- src/main/java/com/umc/linkyou/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/com/umc/linkyou/apiPayload/code/status/ErrorStatus.java
@@ -50,7 +50,7 @@ public enum ErrorStatus implements BaseErrorCode {
     _SOCIAL_PROFILE_EXPIRED(HttpStatus.BAD_REQUEST, "OAUTH4005", "임시 프로필이 만료되었습니다."),
     _SOCIAL_PROFILE_NOT_REQUIRED(HttpStatus.BAD_REQUEST, "OAUTH4006", "임시 프로필이 필요하지 않습니다."),
     _INVALID_AUTH_CODE(HttpStatus.BAD_REQUEST, "OAUTH4007", "웹:유효하지 않거나 만료된 인증 코드"),
-    _INVALID_ID_TOKEN(HttpStatus.BAD_REQUEST, "OAUTH4008", "앱:유효하지 않거나 만료된 인증 코드"),
+    _INVALID_ID_TOKEN(HttpStatus.BAD_REQUEST, "OAUTH4008", "앱:유효하지 않거나 만료된 ID 토큰"),
     //링큐 관련 코드
     _LINKU_VIDEO_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "LINKU4001", "영상 링크는 저장할 수 없습니다."),
     _LINKU_INVALID_URL(HttpStatus.BAD_REQUEST, "LINKU4002", "유효하지 않은 링크입니다."),
