32917d8c5f5f3bfe9ba0d3dd1a4f3d0063fe2b6b
oculo0204 <oculo0204@gmail.com>
2026-03-17 20:24:13 +0900

    refactor: #243 cloud.aws properties combined in properties

--- src/main/java/com/umc/linkyou/config/AwsS3Config.java
+++ src/main/java/com/umc/linkyou/config/AwsS3Config.java
@@ -4,30 +4,30 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.umc.linkyou.config.properties.AwsProperties;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+//awss3 폴더에 연관 파일들 적혀있음
 @Slf4j
 @Configuration
+@RequiredArgsConstructor
 public class AwsS3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
-
-    @Value("${cloud.aws.region.static}")
-    private String region;
+    private final AwsProperties awsProperties;
 
     @Bean
     public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(
+                awsProperties.getCredentials().getAccessKey(),
+                awsProperties.getCredentials().getSecretKey()
+        );
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withRegion(region)
+                .withRegion(awsProperties.getRegion().getStaticRegion())
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .build();
     }
 }
+
