cb59fbe40236e3572705761d02b9487de243a3a7
oculo0204 <oculo0204@gmail.com>
2026-06-16 03:37:15 +0900

    fix: #326 iam role

--- src/main/java/com/umc/linkyou/config/external/AwsS3Config.java
+++ src/main/java/com/umc/linkyou/config/external/AwsS3Config.java
@@ -1,14 +1,13 @@
 package com.umc.linkyou.config.external;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.umc.linkyou.config.properties.AwsProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
 //awss3 폴더에 연관 파일들 적혀있음
 @Slf4j
 @Configuration
@@ -18,15 +17,10 @@ public class AwsS3Config {
     private final AwsProperties awsProperties;
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(
-                awsProperties.getCredentials().getAccessKey(),
-                awsProperties.getCredentials().getSecretKey()
-        );
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withRegion(awsProperties.getRegion().getStaticRegion())
-                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
-                .build();
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(awsProperties.region().staticRegion()))
+                .build(); // EC2 IAM Role 자동 인증
     }
 }
 
