792405470d4baffacb6b47a62cf6ac4ed95d1f83
oculo0204 <oculo0204@gmail.com>
2026-06-23 04:56:41 +0900

    feat: keyword, linkuKeyword 추가

---
+++ src/main/java/com/umc/linkyou/repository/keywordRepository/KeywordRepository.java
@@ -0,0 +1,12 @@
+package com.umc.linkyou.repository.keywordRepository;
+
+import com.umc.linkyou.domain.Keyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+    Optional<Keyword> findByName(String name);
+}
