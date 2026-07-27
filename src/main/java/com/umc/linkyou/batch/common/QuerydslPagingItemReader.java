62282bc8b5d614d021b11f6d428c38feae8d75aa
ljw42b <80021912+JiwonLee42@users.noreply.github.com>
2026-07-28 01:14:56 +0900

    fix: #308 QuerydslPagingItemReader lastId 갱신 시점을 read() 반환 시점으로 변경

--- src/main/java/com/umc/linkyou/batch/common/QuerydslPagingItemReader.java
+++ src/main/java/com/umc/linkyou/batch/common/QuerydslPagingItemReader.java
@@ -4,6 +4,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.NonNull;
 import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemStreamException;
 import org.springframework.batch.item.database.AbstractPagingItemReader;
@@ -14,8 +15,8 @@ import java.util.concurrent.CopyOnWriteArrayList;
 /**
  * Querydsl 기반 no-offset paging reader
  *
- * <p>페이지 번호 대신 마지막으로 읽은 ID를 기준으로 다음 청크를 조회한다.
- * 삭제/상태 변경이 섞인 배치에서도 OFFSET 밀림을 피하기 위한 공통 reader다.
+ * <p>페이지 번호 대신 마지막으로 읽은 ID를 기준으로 다음 청크를 조회
+ * 삭제/상태 변경이 섞인 배치에서도 OFFSET 밀림을 피하기 위한 공통 reader
  */
 @Slf4j
 public abstract class QuerydslPagingItemReader<T> extends AbstractPagingItemReader<T> {
@@ -40,7 +41,7 @@ public abstract class QuerydslPagingItemReader<T> extends AbstractPagingItemRead
     }
 
     @Override
-    public void update(ExecutionContext executionContext) throws ItemStreamException {
+    public void update(@NonNull ExecutionContext executionContext) throws ItemStreamException {
         super.update(executionContext);
         executionContext.putLong(getExecutionContextKey("lastId"), lastId);
     }
@@ -57,7 +58,7 @@ public abstract class QuerydslPagingItemReader<T> extends AbstractPagingItemRead
      */
     @Override
     protected void doReadPage() {
-        // 이전 페이지 엔티티가 남아 있으면 메모리와 영속성 컨텍스트가 불필요하게 커진다
+        // 이전 페이지 엔티티가 남아 있으면 메모리와 영속성 컨텍스트가 불필요하게 커지므로 clear
         results = new CopyOnWriteArrayList<>();
         entityManager.clear();
 
@@ -66,14 +67,7 @@ public abstract class QuerydslPagingItemReader<T> extends AbstractPagingItemRead
         if (queryResult.isEmpty()) {
             return;
         }
-
-        // 다음 페이지는 마지막으로 읽은 ID 다음부터 조회한다
-        lastId = getLastId(queryResult.get(queryResult.size() - 1));
         results.addAll(queryResult);
-    }
-
-    protected void doJumpToPage(int itemIndex) {
-        log.debug("No-offset reader does not support random page jump. itemIndex={}", itemIndex);
     }
 
     @Override
@@ -82,6 +76,17 @@ public abstract class QuerydslPagingItemReader<T> extends AbstractPagingItemRead
             entityManager.close();
         }
         super.doClose();
+    }
+
+
+    // 커밋 시점에 영속화되는 lastId 값을 정확하게 가져올 수 있게 , 한번에 읽어오지 않고 read를 오버라이드 해서 읽어올때 시점에 lastId를 가져옴
+    @Override
+    public T read() throws Exception {
+        T item = super.read();
+        if (item != null) {
+            lastId = getLastId(item);
+        }
+        return item;
     }
 
     protected abstract List<T> fetchQuery(Long lastId, int pageSize);
