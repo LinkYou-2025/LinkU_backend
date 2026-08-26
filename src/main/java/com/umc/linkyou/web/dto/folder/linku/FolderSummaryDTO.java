13a4744e137ac76b167b4602869f138d1d61acc0
ljw42b <80021912+JiwonLee42@users.noreply.github.com>
2026-08-26 16:45:49 +0900

    refactor: 폴더 내부 응답 DTO를 builder로 생성

--- src/main/java/com/umc/linkyou/web/dto/folder/linku/FolderSummaryDTO.java
+++ src/main/java/com/umc/linkyou/web/dto/folder/linku/FolderSummaryDTO.java
@@ -1,10 +1,14 @@
 package com.umc.linkyou.web.dto.folder.linku;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 // 중폴더 내부에 있는 소분류 폴더 응답 형식
 public class FolderSummaryDTO {
     private Long folderId;
